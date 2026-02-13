e10fd184b637da2e174410725f0e3e0a17bdb528
Raunak shahu <raunakshahu.cg@gmail.com>
2026-02-13 11:03:08 +0530

    server

---
+++ 3/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+
+const app = express();
+
+const data = [
+    {
+        "studentName": "ABDUL HAQUE",
+        "University": "SUxCG 714",
+        "UniversityUID": "108444"
+    },
+    {
+        "studentName": "ADITYA KUMAR",
+        "University": "SUxCG 702",
+        "UniversityUID": "108716",
+    },
+    {
+        "studentName": "AMAN KUMAR",
+        "University": "SUxCG 702",
+        "UniversityUID": "108500"
+    },
+    {
+        "studentName": "AMRIT RAJ",
+        "University": "SUxCG 702",
+        "UniversityUID": "108587"
+    },
+    {
+        "studentName": "Jay Patel",
+        "University": "SUxCG 714",
+        "UniversityUID": "108540"
+    },
+
+]
+
+app.get("/", (req, res) => {
+    res.send("Express server is running");
+});
+app.get("/cg/students", (req, res) => {
+    res.send(data);
+});
+app.get("/cg/students/id/:UniversityUID", (req, res) => {
+  const userId = (req.params.UniversityUID);
+  const user = data.find(u => u.UniversityUID === userId);
+
+  if (!user) {
+    return res.status(404).json({ message: "Student not found" });
+  }
+
+  res.status(200).json(user);
+});
+app.get("/cg/students/name/:studentName", (req, res) => {
+  const userId = (req.params.studentName).toLowerCase();
+  const user = data.find(u => u.studentName.toLowerCase() === userId);
+
+  if (!user) {
+    return res.status(404).json({ message: "Student not found" });
+  }
+
+  res.status(200).json(user);
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
--- 4/index.js
+++ 4/index.js
@@ -1,22 +1,17 @@
 const express = require("express");
 
 const app = express();
+
 app.use(express.json());
+
 
 const users = [
   { id: 1, name: "Arjun", role: "student" },
-  { id: 2, name: "Priyesha", role: "mentor" },
-  { id: 3, name: "Harshil", role: "student" },
-  { id: 4, name: "Jay", role: "student" }
+  { id: 2, name: "Priyesha", role: "mentor" }
 ];
 
-
 app.get("/", (req, res) => {
-    res.send("Raunak Shahu!!");
-});
-
-app.get("/second", (req, res) => {
-    res.send("Express server is running2");
+  res.send("Express server is running");
 });
 
 app.get("/users", (req, res) => {
@@ -34,6 +29,7 @@ app.get("/users/:id", (req, res) => {
   res.status(200).json(user);
 });
 
+
 app.post("/users", (req, res) => {
   const newUser = {
     id: users.length + 1,
@@ -47,7 +43,27 @@ app.post("/users", (req, res) => {
     message: "User created",
     user: newUser
   });
-})
+});
+
+app.put("/users/:id", (req, res) => {
+  const userId = Number(req.params.id);
+  const index = users.findIndex(u => u.id === userId);
+
+  if (index === -1) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  users[index] = {
+    id: userId,
+    name: req.body.name,
+    role: req.body.role
+  };
+
+  res.status(200).json({
+    message: "User replaced",
+    user: users[index]
+  });
+});
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
